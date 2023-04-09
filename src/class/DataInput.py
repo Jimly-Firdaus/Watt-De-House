d226a6a92454aaf6c5b44be83012b45d4d965996
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-09 20:57:07 +0700

    feat: data input implementation

--- src/class/DataInput.py
+++ src/class/DataInput.py
@@ -1,11 +1,49 @@
 from interface.IDataInput import *
 
 
-class DataInput(IDataInput):
-    def get_user_input(self):
-        # implementation
-        pass
+class DataInput1(IDataInput):
+    def __init__(self, nama: str, daya: float, arus: float, tegangan: float):
+        self.validate_user_input(nama, daya, arus, tegangan)
 
-    def validate_user_input(self):
-        # implementation
-        pass
+    def validate_user_input(self, nama: str, daya: float, arus: float, tegangan: float):
+        if len(nama) == 0:
+            raise Exception("Nama perangkat listrik tidak boleh kosong!")
+        if daya <= 0:
+            raise Exception("Daya tidak boleh < 0 atau sama dengan 0!")
+        if arus <= 0:
+            raise Exception("Arus tidak boleh < 0 atau sama dengan 0!")
+        if tegangan <= 0:
+            raise Exception("Tegangan tidak boleh < 0 atau sama dengan 0!")
+        self.nama = nama
+        self.daya = daya
+        self.arus = arus
+        self.tegangan = tegangan
+
+    # Getters
+    def get_user_input(self):
+        return (self.nama, self.daya, self.arus, self.tegangan)
+
+    def get_input_name(self):
+        return self.nama
+
+    def get_input_daya(self):
+        return self.daya
+
+    def get_input_arus(self):
+        return self.arus
+
+    def get_input_tegangan(self):
+        return self.tegangan
+
+
+class DataInput2(DataInput1):
+    def __init__(
+        self, nama: str, daya: float, arus: float, tegangan: float, durasi: int
+    ):
+        super().__init__(nama, daya, arus, tegangan)
+        if self.durasi < 0:
+            raise Exception("Durasi tidak boleh < 0")
+        self.durasi = durasi
+
+    def get_user_input(self):
+        return (self.nama, self.daya, self.arus, self.tegangan, self.durasi)
