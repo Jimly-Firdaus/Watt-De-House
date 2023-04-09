47400d9d5af6c3f84b88d3bf94db018dd67d5171
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-09 21:10:37 +0700

    fix: update unittest

--- test/test_module.py
+++ test/test_module.py
@@ -1,12 +1,31 @@
 import sys
-sys.path.append('..')
+
+sys.path.append("..")
 import unittest
-from src.main import pangkat2
+from src.ClassFiles.DataInput import DataInput1, DataInput2
+
 
 class ModuleTesting(unittest.TestCase):
-    def test_pangkat2(self):
-        self.assertEqual(pangkat2(2), 4)
+    def test_data_input_1(self):
+        # Test valid input
+        data_input = DataInput1("Device-1", 100.0, 5.0, 220.0)
+        self.assertEqual(data_input.get_user_input(), ("Device-1", 100.0, 5.0, 220.0))
 
-if __name__ == '__main__':
+        # Test invalid input
+        with self.assertRaises(Exception):
+            data_input = DataInput1("", 100.0, 5.0, 220.0)
+
+    def test_data_input_2(self):
+        # Test valid input
+        data_input = DataInput2("Device-2", 100.0, 5.0, 220.0, 10)
+        self.assertEqual(
+            data_input.get_user_input(), ("Device-2", 100.0, 5.0, 220.0, 10)
+        )
+
+        # Test invalid input
+        with self.assertRaises(Exception):
+            data_input = DataInput2("Device-2", 100.0, 5.0, 220.0, -1)
+
+
+if __name__ == "__main__":
     unittest.main()
-    
