c91f4812f8b638325454f2bec8c79ce01e761706
billc27 <110593711+billc27@users.noreply.github.com>
2023-04-09 13:44:26 +0700

    added PerangkatListrik class

---
+++ src/PerangkatListrik/PerangkatListrik.py
@@ -0,0 +1,32 @@
+# File PerangkatListrik.py
+
+
+class PerangkatListrik:
+    # Constructor
+    def __init__(self):
+        self.nama_p_listrik = ""
+        self.daya_p_listrik = 0
+        self.arus_p_listrik = 0
+        self.tegangan_p_listrik = 0
+
+    # Getter
+    def getDataPerangkatListrik(self):
+        return (
+            self.nama_p_listrik,
+            self.daya_p_listrik,
+            self.arus_p_listrik,
+            self.tegangan_p_listrik,
+        )
+
+    # Setter
+    def setNamaPListrik(self, name):
+        self.nama_p_listrik = name
+
+    def setDayaPListrik(self, daya):
+        self.daya_p_listrik = daya
+
+    def setArusPListrik(self, arus):
+        self.arus_p_listrik = arus
+
+    def setTeganganPListrik(self, tegangan):
+        self.tegangan_p_listrik = tegangan
