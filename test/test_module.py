170159d323a01dc7368743b336c83fe1c16bfcc4
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-10 20:54:37 +0700

    fix: update unittest

--- test/test_module.py
+++ test/test_module.py
@@ -1,12 +1,55 @@
 import sys
-sys.path.append('..')
+
+sys.path.append("..")
 import unittest
-from src.main import pangkat2
+from src.ClassFiles.Database import Database
+
 
 class ModuleTesting(unittest.TestCase):
-    def test_pangkat2(self):
-        self.assertEqual(pangkat2(2), 4)
+    def test_insert_data(self):
+        db = Database("test_table")
 
-if __name__ == '__main__':
+        db.create_table(
+            "test_table",
+            {
+                "nama": "text",
+                "daya": "real",
+                "arus": "real",
+                "tegangan": "real",
+                "durasi": "integer",
+            },
+        )
+        db.insert_data(
+            "test_table",
+            {
+                "nama": "device-1",
+                "daya": "100.0",
+                "arus": "12.0",
+                "tegangan": "220.0",
+                "durasi": "120",
+            },
+        )
+        data = db.get_data("test_table")
+        expected_data = [("device-1", 100.0, 12.0, 220.0, 120)]
+
+        self.assertEqual(data, expected_data)
+
+    def test_update_data(self):
+        db = Database("test_table")
+        db.update_data("test_table", {"nama": "device-2"}, {"nama": "device-1"})
+        data = db.get_data("test_table")
+        expected_data = [("device-2", 100.0, 12.0, 220.0, 120)]
+
+        self.assertEqual(data, expected_data)
+
+    def test_delete_data(self):
+        db = Database("test_table")
+        db.delete_data("test_table", {"arus": "12.0"})
+        data = db.get_data("test_table")
+        expected_data = []
+
+        self.assertEqual(data, expected_data)
+
+
+if __name__ == "__main__":
     unittest.main()
-    
