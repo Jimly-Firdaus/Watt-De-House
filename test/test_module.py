5ebcd0c0938e4ebcd617756bba10a886492a06e9
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-10 22:37:10 +0700

    fix: move to subtest for ordering

--- test/test_module.py
+++ test/test_module.py
@@ -12,42 +12,45 @@ class ModuleTesting(unittest.TestCase):
     def tearDown(self):
         self.db.close()
 
-    def test_1_insert_data(self):
-        self.db.create_table(
-            "test_table",
-            {
-                "nama": "text",
-                "daya": "real",
-                "arus": "real",
-                "tegangan": "real",
-                "durasi": "integer",
-            },
-        )
-        self.db.insert_data(
-            "test_table",
-            {
-                "nama": "device-1",
-                "daya": 100.0,
-                "arus": 12.0,
-                "tegangan": 220.0,
-                "durasi": 120,
-            },
-        )
-        data = self.db.get_data("test_table")
-        expected_data = [("device-1", 100.0, 12.0, 220.0, 120)]
-        self.assertEqual(data, expected_data)
+    def test_database_operations(self):
+        with self.subTest("Insert data"):
+            self.db.create_table(
+                "test_table",
+                {
+                    "nama": "text",
+                    "daya": "real",
+                    "arus": "real",
+                    "tegangan": "real",
+                    "durasi": "integer",
+                },
+            )
+            self.db.insert_data(
+                "test_table",
+                {
+                    "nama": "device-1",
+                    "daya": 100.0,
+                    "arus": 12.0,
+                    "tegangan": 220.0,
+                    "durasi": 120,
+                },
+            )
+            data = self.db.get_data("test_table")
+            expected_data = [("device-1", 100.0, 12.0, 220.0, 120)]
+            self.assertEqual(data, expected_data)
 
-    def test_2_update_data(self):
-        self.db.update_data("test_table", {"nama": "device-2"}, {"nama": "device-1"})
-        data = self.db.get_data("test_table")
-        expected_data = [("device-2", 100.0, 12.0, 220.0, 120)]
-        self.assertEqual(data, expected_data)
+        with self.subTest("Update data"):
+            self.db.update_data(
+                "test_table", {"nama": "device-2"}, {"nama": "device-1"}
+            )
+            data = self.db.get_data("test_table")
+            expected_data = [("device-2", 100.0, 12.0, 220.0, 120)]
+            self.assertEqual(data, expected_data)
 
-    def test_3_delete_data(self):
-        self.db.delete_data("test_table", {"arus": 12.0})
-        data = self.db.get_data("test_table")
-        expected_data = []
-        self.assertEqual(data, expected_data)
+        with self.subTest("Delete data"):
+            self.db.delete_data("test_table", {"arus": 12.0})
+            data = self.db.get_data("test_table")
+            expected_data = []
+            self.assertEqual(data, expected_data)
 
 
 if __name__ == "__main__":
