db7a8aad7bd03f874e10b5ed1cc2ca672ec739f4
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-15 12:10:03 +0700

    feat: page link function

--- src/UI/pages/MainPageUI.py
+++ src/UI/pages/MainPageUI.py
@@ -10,6 +10,7 @@ class MainFrame(PageWindow):
         self.setBaseSize(1024, 720)
         self.setSizeIncrement(2, 2)
         self.setStyleSheet("background-color: #0B2447;")
+        self.setWindowTitle("Watt de House")
 
         widget = QtWidgets.QWidget()
         self.setCentralWidget(widget)
@@ -20,7 +21,7 @@ class MainFrame(PageWindow):
         # Exit button section
         h1_layout = QtWidgets.QHBoxLayout()
         h1_layout.addStretch()
-        self.exitButton = UtilityButton("Exit", self)
+        self.exitButton = UtilityButton("Exit", None, self)
         self.exitButton.setMinimumSize(90, 90)
         h1_layout.addWidget(self.exitButton)
         v_layout.addLayout(h1_layout)
@@ -30,12 +31,12 @@ class MainFrame(PageWindow):
         h2_layout = QtWidgets.QHBoxLayout()
         v_layout.addLayout(h2_layout)
         h2_layout.addStretch()
-        self.a1 = FeatureButton("Estimator", self)
+        self.a1 = FeatureButton("Estimator", None, self)
         self.a1.setMinimumSize(210, 100)
         h2_layout.addWidget(self.a1)
         h2_layout.addStretch()
 
-        self.a2 = FeatureButton("Simulator", self)
+        self.a2 = FeatureButton("Simulator", lambda: self.moveToPage("simulator"), self)
         self.a2.setMinimumSize(210, 100)
         h2_layout.addWidget(self.a2)
         h2_layout.addStretch()
@@ -44,7 +45,12 @@ class MainFrame(PageWindow):
         # Help button section
         h3_layout = QtWidgets.QHBoxLayout()
         h3_layout.addStretch()
-        self.helpButton = UtilityButton("Help", self)
+        self.helpButton = UtilityButton("Help", None, self)
         self.helpButton.setMinimumSize(90, 90)
         h3_layout.addWidget(self.helpButton)
         v_layout.addLayout(h3_layout)
+
+    # Callback fn
+    def moveToPage(self, page_name):
+        if page_name == "simulator":
+            self.goto("simulator")
