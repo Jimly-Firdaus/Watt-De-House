2a37380be17c1dbcb7fa28b47bce3cc13e5f5204
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-15 09:52:54 +0700

    refactor: move main page, replace with main window

--- src/UI/app.py
+++ src/UI/app.py
@@ -1,52 +1,53 @@
-import sys
-from PyQt5 import QtGui, QtCore, QtWidgets
+from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
-from components.FeatureButton import FeatureButton
-from components.UtilityButton import UtilityButton
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QDesktopWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget
 from components.BaseCard import BaseCard
+from pages.MainPageUI import MainFrame
 
 
-class MainFrame(QWidget):
-    def __init__(self):
-        super().__init__()
+class Window(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
         self.setGeometry(0, 0, 1440, 810)
-        self.setStyleSheet(
-            """
-            background-color: #0B2447;
-        """
-        )
+        self.stacked_widget = QtWidgets.QStackedWidget()
+        self.setCentralWidget(self.stacked_widget)
 
-        # layout
+        # Center the opened window
         screen = QDesktopWidget().screenGeometry()
         x = (screen.width() - self.width()) // 2
         y = (screen.height() - self.height()) // 2
         self.move(x, y)
 
-        # Main Content Here
+        # Pages here
+        self.m_pages = {}
 
-        # Create a BaseButton and add it to the layout
-        a1 = FeatureButton("Estimator", self)
-        a1.set_size(210, 100)
-        a1.set_position(440, 350)
+        # Register your page here
+        self.register(MainFrame(), "main")
+        # e.g: self.register(SearchWindow(), "search")
 
-        a2 = FeatureButton("Simulator", self)
-        a2.set_size(210, 100)
-        a2.set_position(770, 350)
+        # Defaults to main
+        self.goto("main")
 
-        exitButton = UtilityButton("Exit", self)
-        exitButton.set_size(90, 90)
-        exitButton.set_position(1300, 30)
+    def register(self, widget, name):
+        self.m_pages[name] = widget
+        self.stacked_widget.addWidget(widget)
+        if isinstance(widget, QWidget):
+            widget.gotoSignal.connect(self.goto)
 
-        helpButton = UtilityButton("Help", self)
-        helpButton.set_size(90, 90)
-        helpButton.set_position(1300, 650)
+    @QtCore.pyqtSlot(str)
+    def goto(self, name):
+        if name in self.m_pages:
+            widget = self.m_pages[name]
+            self.stacked_widget.setCurrentWidget(widget)
+            self.setWindowTitle(widget.windowTitle())
 
 
 if __name__ == "__main__":
+    import sys
+
     app = QApplication(sys.argv)
-    window = MainFrame()
+    window = Window()
 
     window.show()
     sys.exit(app.exec_())
