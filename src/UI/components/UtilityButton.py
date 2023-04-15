4881136563b456feb59d78152a1412f417239d61
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-15 12:09:38 +0700

    feat: add callback fn to btn

--- src/UI/components/UtilityButton.py
+++ src/UI/components/UtilityButton.py
@@ -2,8 +2,8 @@ from components.BaseButton import BaseButton
 
 
 class UtilityButton(BaseButton):
-    def __init__(self, text, parent=None):
-        super().__init__(text, parent)
+    def __init__(self, text, on_click_callback=None, parent=None):
+        super().__init__(text, on_click_callback, parent)
         self.setStyleSheet(
             """
             UtilityButton {
