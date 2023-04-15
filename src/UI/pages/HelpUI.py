3c06612b31a8050bade86b55a8a427090d17fd42
Ulung32 <13521122@mahasiswa.itb.ac.id>
2023-04-16 00:07:18 +0700

    feat: HelpUI

--- src/UI/pages/HelpUI.py
+++ src/UI/pages/HelpUI.py
@@ -20,13 +20,22 @@ class HelpPage(PageWindow):
         widget = QtWidgets.QWidget()
         self.setCentralWidget(widget)
 
-        self.label = QtWidgets.QLabel("hello world", self)
+        self.label = QtWidgets.QLabel("Help", self)
         self.label.setAlignment(Qt.AlignCenter)
-        self.label.setStyleSheet("color: red; font-size: 30px;")
+        self.label.setStyleSheet("color: #FEFAE0;font-size: 30px; weight: bold")
+
+        self.text = QtWidgets.QTextEdit(self)
+        self.text.setReadOnly(True)
+        self.text.setAlignment(Qt.AlignJustify)
+        self.text.setStyleSheet(
+            "font-size: 25px; color: #FEFAE0; background-color: #0B2447; height: 200px; width: 500px;"
+        )
+
+        text = "1. Klik simulator untuk menjalakan fitur simulator \n2. Klik Estimator untuk menjalakan fitur estimator\n"
+        self.text.setText(text)
 
         self.BackButton = UtilityButton("Back", lambda: self.back_to_main(), self)
         self.BackButton.setMinimumSize(90, 90)
-        self.BackButton.clicked.connect(self.back_to_main)
 
         v_layout = QtWidgets.QVBoxLayout(widget)
 
@@ -34,14 +43,20 @@ class HelpPage(PageWindow):
         h1_layout.addStretch()
         h1_layout.addWidget(self.BackButton)
 
-        v_layout.addStretch(5)
+        v_layout.addStretch(3)
         h2_layout = QtWidgets.QHBoxLayout()
         h2_layout.addStretch()
 
         h2_layout.addWidget(self.label)
+        # h2_layout.addWidget(self.label1)
         h2_layout.addStretch()
         v_layout.addLayout(h2_layout)
 
+        h3_layout = QtWidgets.QHBoxLayout(widget)
+        h3_layout.addStretch()
+        h3_layout.addWidget(self.text)
+        h3_layout.addStretch()
+        v_layout.addLayout(h3_layout)
         # Create the button and set its properties
 
         v_layout.addStretch(5)
