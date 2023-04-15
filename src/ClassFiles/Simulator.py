540e27f7ef1f305163201fc3dd1f87097be93f7b
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-15 16:22:56 +0700

    fix: compute shareable circuit breakers

--- src/ClassFiles/Simulator.py
+++ src/ClassFiles/Simulator.py
@@ -10,26 +10,40 @@ class Simulator:
         self.electrical_overload = False
         self.notify_button = False
 
-    def update_electrical_state(self, list_ruangan):
+    def update_electrical_state(self, list_ruangan: List[Ruangan]):
         self.overloads_ruangan = []
+        circuit_breaker_power = {}
         for ruangan in list_ruangan:
-            if (
-                ruangan.total_power_used() > ruangan.get_threshold()
-                and ruangan.have_circuit_breaker()
-            ):
-                self.overloads_ruangan.append((ruangan.get_ruangan_name(), True))
+            if ruangan.have_circuit_breaker():
+                circuit_breaker_name = ruangan.get_circuit_breaker_name()
+                if circuit_breaker_name not in circuit_breaker_power:
+                    circuit_breaker_power[circuit_breaker_name] = 0
+                circuit_breaker_power[
+                    circuit_breaker_name
+                ] += ruangan.total_power_used()
 
-    def get_simulator_state(self, list_ruangan):
+        for ruangan in list_ruangan:
+            if ruangan.have_circuit_breaker():
+                circuit_breaker_name = ruangan.get_circuit_breaker_name()
+                if (
+                    circuit_breaker_power[circuit_breaker_name]
+                    > ruangan.get_threshold()
+                ):
+                    self.overloads_ruangan.append(
+                        (ruangan.get_ruangan_name(), True, True)
+                    )
+
+    def get_simulator_state(self, list_ruangan: List[Ruangan]):
         self.update_electrical_state(list_ruangan)
         if len(self.overloads_ruangan) != 0:
-            return (True, self.overloads_ruangan)
-        return (False, self.overloads_ruangan)
+            return self.overloads_ruangan
+        return []
 
     def stop_simulation(self):
         if self.notify_button:
             del self
 
-    def get_estimator(self, list_ruangan):
+    def get_estimator(self, list_ruangan: List[Ruangan]):
         list_estimator = []
         for ruangan in list_ruangan:
             list_perangkat_listrik_ruangan = ruangan.get_list_perangkat_listrik()
