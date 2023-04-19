cffa6d75001b38b90c0b489ecd3c5d04c2786392
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-19 21:15:05 +0700

    fix: fix test module

--- test/test_module.py
+++ test/test_module.py
@@ -123,10 +123,12 @@ class ModuleTesting(unittest.TestCase):
         listPerangkat.append(perangkat_listrik1)
         Estimator1 = Estimator(True, listPerangkat, 605)
         self.assertEqual(
-            Estimator1.get_harga_listrik(), (605),
+            Estimator1.get_harga_listrik(),
+            (605),
         )
         self.assertEqual(
-            Estimator1.get_total_biaya(), (2 * 30 * 605),
+            Estimator1.get_total_biaya(),
+            (2 * 30 * 605),
         )
 
     def test_ruangan(self):
@@ -155,7 +157,8 @@ class ModuleTesting(unittest.TestCase):
 
     def test_run_gui(self):
         try:
-            main.run_app()
+            app = main.run_app()
+            app.quit()
         except Exception as e:
             self.fail(f"Running the app raised an exception: {e}")
 
