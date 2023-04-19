85de4fa4815214575109709940b7c8458aafcef7
billc27 <110593711+billc27@users.noreply.github.com>
2023-04-19 23:03:37 +0700

    fix: delete unused code

--- src/UI/app.py
+++ src/UI/app.py
@@ -96,7 +96,6 @@ class Window(QtWidgets.QMainWindow):
         main_page.list_ruangan_updated.connect(simulator_page.update_self_list)
         main_page.list_ruangan_updated.connect(house_frame.update_self_list)
         house_frame.list_ruangan_updated.connect(main_page.update_self_list)
-        main_page.reset.connect(house_frame.reset_self_list)
 
         # Defaults to main
         self.goto("main")
